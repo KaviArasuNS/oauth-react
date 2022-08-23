1e598eb0accda990f7963c472688c29b3c7ceff5
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-23 17:55:52 +0530

    oauth social

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import "./app.css";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("authentication has been failed!");
+        })
+        .then((resObject) => {
+          setUser(resObject.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getUser();
+  }, []);
+
+  console.log(user);
+  return (
+    <div>
+      <Navbar user={user} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/post/:id"
+          element={user ? <Post /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Google =
+  "https://raw.githubusercontent.com/safak/youtube/react-social-login/client/src/img/google.png";
+const Facebook =
+  "https://raw.githubusercontent.com/safak/youtube/react-social-login/client/src/img/facebook.png";
+const Github =
+  "https://raw.githubusercontent.com/safak/youtube/react-social-login/client/src/img/github.png";
+
+const Login = () => {
+  const google = () => {
+    window.open("http://localhost:5000/auth/google", "_self");
+  };
+
+  const github = () => {
+    window.open("http://localhost:5000/auth/github", "_self");
+  };
+
+  const facebook = () => {
+    window.open("http://localhost:5000/auth/facebok", "_self");
+  };
+  return (
+    <div className="login">
+      <h1 className="loginTitle">Choose a Login Method</h1>
+      <div className="wrapper">
+        <div className="left">
+          <div className="loginButton google" onClick={google}>
+            <img src={Google} alt="" className="icon" />
+            Google
+          </div>
+          <div className="loginButton facebook" onClick={facebook}>
+            <img src={Facebook} alt="" className="icon" />
+            Facebook
+          </div>
+          <div className="loginButton github" onClick={github}>
+            <img src={Github} alt="" className="icon" />
+            Github
+          </div>
+        </div>
+        <div className="center">
+          <div className="line" />
+          <div className="or">OR</div>
+        </div>
+        <div className="right">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button className="submit">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
